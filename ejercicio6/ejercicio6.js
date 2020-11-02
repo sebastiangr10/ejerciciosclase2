88a7f7830f07e8f27f7927ca93285d8c49a3b90f
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-02 17:20:38 -0500

    ejercicio7

--- ejercicio6/ejercicio6.js
+++ ejercicio6/ejercicio6.js
@@ -2,6 +2,6 @@ let numero
 let suma10
 numero=Number(prompt("ingrese un número"))
 suma10=numero+10
-alert("su numero mas 10 es igual a " + suma10)
-document.write("su numero mas 10 es igual a " + suma10)
-console.log("su numero mas 10 es igual a " + suma10)
+alert("su número mas 10 es igual a " + suma10)
+document.write("su número mas 10 es igual a " + suma10)
+console.log("su número mas 10 es igual a " + suma10)
---
+++ ejercicio7/ejercicio7.js
@@ -0,0 +1,7 @@
+let grados;
+let fahrenheit;
+grados=Number(prompt("ingrese grados centigrados"))
+fahrenheit=(grados*1.8)+32
+alert(grados + "grados centigrados convertidos a fahrenheit es igual a" + fahrenheit + "grados fahrenheit")
+document.write(grados + "grados centigrados convertidos a fahrenheit es igual a" + fahrenheit + "grados fahrenheit")
+console.log(grados + "grados centigrados convertidos a fahrenheit es igual a" + fahrenheit + "grados fahrenheit")
