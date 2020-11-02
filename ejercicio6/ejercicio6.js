e715919ee014debad45c587de4132ce593825564
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-02 16:57:09 -0500

    ejercicio6

--- ejercicio6/ejercicio6.js
+++ ejercicio6/ejercicio6.js
@@ -3,3 +3,5 @@ let suma10
 numero=Number(prompt("ingrese un número"))
 suma10=numero+10
 alert("su numero mas 10 es igual a " + suma10)
+document.write("su numero mas 10 es igual a " + suma10)
+console.log("su numero mas 10 es igual a " + suma10)
