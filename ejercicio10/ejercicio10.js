6d1556acb6a4e9522961707bdb7b2c18e9adb864
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-02 18:08:00 -0500

    ejercicio10

---
+++ ejercicio10/ejercicio10.js
@@ -0,0 +1,9 @@
+let base;
+let altura;
+let area;
+base=Number(prompt("ingrese la medida de la base del rectangulo" ));
+altura=Number(prompt("ingrese la altura del rectangulo"));
+area= base*altura;
+alert("el area del rectangulo es " + area);
+document.write("el area del rectangulo es " + area);
+console.log("el area del rectangulo es " + area);
