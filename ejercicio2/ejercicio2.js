75a7e9ea2f65850f2458d127fc2ea8ebdca39feb
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-02 15:37:52 -0500

    ejercicio2

---
+++ ejercicio2/ejercicio2.js
@@ -0,0 +1,10 @@
+let num1;
+let num2;
+let multiplicacion
+num1=Number(prompt("ingresa primer numero"));
+num2=Number(prompt("ingresa segundo numero"));
+multiplicacion=num1*num2
+alert("el resultado de la multiplicacion es " + multiplicacion)
+
+document.write("el resultado de la multiplicacion es " + multiplicacion);
+console.log("el resultado de la multiplicacion es " + multiplicacion)
