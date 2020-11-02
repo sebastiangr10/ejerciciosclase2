343d8b615c1f549f54cd4e61a2fd0059e8a907f1
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-02 14:39:44 -0500

    ejercicio1

---
+++ ejercicio1/ejercicio.js
@@ -0,0 +1,10 @@
+let num1;
+let num2;
+let suma=0
+num1=Number(prompt("ingresa primer numero"));
+num2=Number(prompt("ingresa segundo numero"));
+suma=num1+num2
+alert("la suma es " + suma)
+
+document.write("la suma es " + suma);
+console.log("la suma es " + suma)
