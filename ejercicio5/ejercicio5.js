40e05c718bd9de6d9d6ac822a44c1d83964a70c1
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-02 16:38:15 -0500

    ejercicio5

---
+++ ejercicio5/ejercicio5.js
@@ -0,0 +1,7 @@
+let grados;
+let fahrenheit;
+grados=30;
+fahrenheit=(grados*1.8)+32;
+alert("30 grados centigrados covertidos a fahrenheit es igual a " +fahrenheit+ "grados fahrenheit") 
+document.write("30 grados centigrados covertidos a fahrenheit es igual a " +fahrenheit+ "grados fahrenheit") 
+console.log("30 grados centigrados covertidos a fahrenheit es igual a " +fahrenheit+ "grados fahrenheit") 
