2eedfb1f438d1811ad0f6fab80b9e88fb81aba11
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-02 17:52:40 -0500

    ejercicio9

---
+++ ejercicio9/ejercicio9.js
@@ -0,0 +1,7 @@
+let precioInicial;
+let precioFinal;
+precioInicial=Number(prompt("ingrese el precio del producto"))
+precioFinal=parseInt(precioInicial*1.19)
+alert("el precio del producto con IVA es " + precioFinal + "$")
+document.write("el precio del producto con IVA es " + precioFinal + "$")
+console.log("el precio del producto con IVA es " + precioFinal + "$")
