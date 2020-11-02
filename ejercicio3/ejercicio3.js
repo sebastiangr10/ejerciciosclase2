3b54ed85202acc73a2d1e7b1dfd8e6628ea3a63d
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-02 16:12:01 -0500

    ejercicio4

--- ejercicio3/ejercicio3.js
+++ ejercicio3/ejercicio3.js
@@ -5,7 +5,7 @@ let edad;
 añoActual=Number(prompt("ingresa año actual"));
 añoNacimiento=Number(prompt("ingresa tu año de nacimiento"));
 edad=añoActual-añoNacimiento;
-alert("su edad es " + edad)
+alert("su edad es " + edad + "años ")
 
-document.write("su edad es " + edad);
-console.log("su  edad es " + edad)
+document.write("su edad es " + edad + "años ");
+console.log("su  edad es " + edad + "años ")
---
+++ ejercicio4/ejercicio4.js
@@ -0,0 +1,11 @@
+let nota1;
+let nota2;
+let nota3;
+let promedio;
+ nota1=4.3;
+ nota2=3.5;
+ nota3=2.8;
+ promedio=(nota1+nota2+nota3)/3;
+ alert(" su promedio es " + promedio)
+ document.write("su promedio es " + promedio);
+console.log("su promedio es " + promedio)
