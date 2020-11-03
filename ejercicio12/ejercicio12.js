a623fcacfa71e25dd9099dcbdca62833d1612db7
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-03 14:53:20 -0500

    ejercicio12

---
+++ ejercicio12/ejercicio12.js
@@ -0,0 +1,9 @@
+let base;
+let altura;
+let area;
+base=Number(prompt("ingrese base del triángulo"));
+altura=Number(prompt("ingrese altura del triángulo"));
+area=(base*altura)/2;
+alert("el area del triángulo es " + area)
+document.write("el area del triángulo es " + area)
+console.log("el area del triángulo es " + area)
