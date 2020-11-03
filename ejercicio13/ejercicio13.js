dc5dbe23724f7e3cfdf46b20258c1e9c97e29940
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-03 15:08:20 -0500

    ejercicio13

---
+++ ejercicio13/ejercicio13.js
@@ -0,0 +1,10 @@
+let metros;
+let pulgadas;
+let medida;
+ 
+metros=Number(prompt("ingrese los metros"));
+pulgadas=0.0254;
+medida=metros/pulgadas;
+alert("la medida en pulgadas es " + medida)
+document.writealert("la medida en pulgadas es " + medida)
+console.logalert("la medida en pulgadas es " + medida)
