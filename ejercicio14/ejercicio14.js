333c754836f9d6943785141f45a045bd63a39d14
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-03 15:20:42 -0500

    ejercicio14

---
+++ ejercicio14/ejercicio14.js
@@ -0,0 +1,10 @@
+let valorHora;
+let horas;
+let cobroTotal;
+
+valorHora=Number(prompt("ingrese el valor de la hora"));
+horas=Number(prompt("ingrese el número de horas en el estacionamiento"));
+cobroTotal=valorHora*horas;
+alert("el  cobro total por el estacionamiento es " + cobroTotal)
+document.write("el  cobro total por el estacionamiento es " + cobroTotal)
+console.log("el  cobro total por el estacionamiento es " + cobroTotal)
