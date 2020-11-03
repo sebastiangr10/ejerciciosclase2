3583cb96dc5bbd0c180f9cca62c0e5bac2aa69c5
sebastiangr10 <sebastiangr1091@gmail.com>
2020-11-03 14:43:34 -0500

    ejercicio11

---
+++ ejercicio11/ejercicio11.js
@@ -0,0 +1,9 @@
+let radio;
+let pi;
+let area;
+radio=Number(prompt("ingrese el radio de la circunferencia"));
+pi=3.1416;
+area=(pi* radio**2);
+alert("el area de la circunferencia es " + area)
+document.write("el area de la circunferencia es " + area)
+console.log("el area de la circunferencia es " + area)
